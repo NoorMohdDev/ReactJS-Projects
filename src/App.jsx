18549c4a5ec37226b924f82c602fda5033ab3c04
NoorMohdDev <noormohddev@gmail.com>
2025-08-02 14:01:10 +0530

    add collapse functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
 import Accordian from "./components/Accordian/Accordian.jsx"
 
 function App() {
+  const setCollapse = (e) => {
 
+    e.target.parentElement.parentElement.parentElement.childNodes.forEach(element => {
+     if (!element.classList.contains("overflow-hidden", "h-10"))
+      element.classList.add("overflow-hidden", "h-10")
+    })
+
+    if (e.target.parentElement.parentElement.classList.contains("overflow-hidden", "h-10")) {
+      e.target.parentElement.parentElement.classList.remove("overflow-hidden", "h-10")
+    } else e.target.parentElement.parentElement.classList.add("overflow-hidden", "h-10")
+
+  }
   return (
     <div className="p-4 box-border">
-     <Accordian/>
+      <Accordian setCollapse={setCollapse} />
     </div>
   )
 }
--- src/components/Accordian/Accordian.jsx
+++ src/components/Accordian/Accordian.jsx
@@ -1,18 +1,20 @@
-import { data } from "./data.js"
 import { useState } from "react"
-function Accordian() {
-  const [collapesed, setCollapesed] = useState(false)
-
+import { data } from "./data.js"
+function Accordian({setCollapse}) {
+  const [faq, setFAQ] = useState([...data])
 
   return (
-    data.map(({ question, answer }, index) => (<div className={ 
-      `p-4 transition-[height] ${collapesed ? "" : "overflow-hidden h-10"}`
-    } key={index}
+    faq.map(({ question, answer }, index) => (<div className="p-4 overflow-hidden h-10" key={index}
     >
-      <div onClick={()=>{setCollapesed(!collapesed)}} className=" cursor-pointer flex justify-between"><h1>{question}</h1>
+      <div className=" cursor-pointer flex justify-between" onClick={setCollapse} >
+        
+        <h1>{question}</h1>
+       
         <span>+</span></div>
       <div className="pt-3 "><p >{answer}</p></div>
-    </div>))
+    </div>
+    )
+    )
   )
 }
 
